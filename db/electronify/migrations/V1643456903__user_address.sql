552a5c9c2d196a036372896f3d991c6232630fb5
Michail Angelos Georgiou <michailangelos.georgiou@gmail.com>
2022-01-29 17:26:12 +0200

    get address by user id view

--- db/electronify/migrations/V1643456903__user_address.sql
+++ db/electronify/migrations/V1643456903__user_address.sql
@@ -1,2 +1,8 @@
 ALTER TABLE electronify.users_address
     ADD COLUMN name VARCHAR(50);
+
+insert into electronify.users_address (id, address_1, address_2, city, postal_code, country, telephone, name)
+values ('04ab814f-68e0-477a-a525-b281670d29ec', 'Kallistratous 3A', '','Limassol','3060',
+        'Cyprus','97639337', 'Theodora');
+
+delete from electronify.users_address where name = 'Theodora'
---
+++ db/electronify/views/R__1643469806_v_get_address_user_id.sql
@@ -0,0 +1,2 @@
+create or replace view public.get_address_by_user_id as
+select * from electronify.users_address where id = 'b7707ae5-9aac-4c21-a50f-c98244a52cb4';

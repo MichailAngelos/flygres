f263a540219f304bdb217f2766111cdbb855c0e0
Michail Angelos Georgiou <michailangelos.georgiou@gmail.com>
2022-04-20 12:42:51 +0300

    fix foreign key , remove unnecessary table and insert data

---
+++ db/electronify/migrations/V1650440767__insert_product.sql
@@ -0,0 +1,53 @@
+ALTER TABLE electronify.product_category
+    ALTER COLUMN create_at TYPE varchar(100);
+
+ALTER TABLE electronify.product_category
+    ALTER COLUMN deleted_at TYPE varchar(100);
+
+ALTER TABLE electronify.product_stock
+    ALTER COLUMN create_at TYPE varchar(100);
+
+ALTER TABLE electronify.product_stock
+    ALTER COLUMN deleted_at TYPE varchar(100);
+
+ALTER TABLE electronify.product
+    ALTER COLUMN created_at TYPE varchar(100);
+
+ALTER TABLE electronify.product_sale
+    ALTER COLUMN deleted_at TYPE varchar(100);
+
+ALTER TABLE electronify.product_sale
+    ALTER COLUMN create_at TYPE varchar(100);
+
+select *
+from electronify.product_sale;
+
+INSERT INTO electronify.product_category (id, name, create_at, deleted_at,product_id)
+VALUES ('1', 'Consoles', '2022-04-20 10:10:25-07', '', '332a2604-c07e-11ec-9d64-0242ac120002');
+INSERT INTO electronify.product_stock (id, quantity, create_at, deleted_at)
+VALUES ('332a2604-c07e-11ec-9d64-0242ac120002', 20, '2022-04-20 10:10:25-07', '');
+
+ALTER TABLE electronify.product_sale
+    Add column product_id varchar(100) references electronify.product_stock (id);
+ALTER TABLE electronify.product_sale
+    ADD CONSTRAINT product_stock foreign key (product_id) references electronify.product_stock (id);
+
+INSERT INTO electronify.product_sale (id, name, discount, active, create_at, deleted_at, product_id)
+VALUES ('332a2604-c07e-11ec-9d64-0242ac120002', 'PS5', 0, false, '2022-04-20 10:10:25-07',
+        '332a2604-c07e-11ec-9d64-0242ac120002');
+
+
+INSERT INTO electronify.product_category(id, name, create_at, deleted_at)  values ('1','Consoles','2022-04-20 10:10:25-07','');
+INSERT INTO electronify.product_category(id, name, create_at, deleted_at)  values ('2','PC','2022-04-20 10:10:25-07','');
+INSERT INTO electronify.product_category(id, name, create_at, deleted_at)  values ('3','Electronics','2022-04-20 10:10:25-07','');
+INSERT INTO electronify.product_category(id, name, create_at, deleted_at)  values ('4','Accessories','2022-04-20 10:10:25-07','');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock)
+VALUES ('332a2604-c07e-11ec-9d64-0242ac120002', 'PS5', 'SONY', 800.0, false, true,'2022-04-20 10:10:25-07','1',0,20 );
+
+ALTER table electronify.product
+    add column sale integer;
+ALTER table electronify.product
+    add column stock integer;
+
+drop table electronify.product_stock;
+drop table electronify.product_sale;
---
+++ db/electronify/migrations/V1650444182__fix_fk.sql
@@ -0,0 +1,36 @@
+Alter table electronify.product_category
+    drop column product_id;
+
+ALTER TABLE electronify.users_address
+    Add column user_id varchar(100) references electronify.users (id);
+ALTER TABLE electronify.shopping_session
+    Add column user_id varchar(100) references electronify.users (id);
+ALTER TABLE electronify.product_stock
+    Add column product varchar(100) references electronify.product (id);
+ALTER TABLE electronify.product_category
+    Add column product_id varchar(100) references electronify.product (id);
+
+ALTER TABLE electronify.product
+    Add column sale_id varchar(100) references electronify.product_sale (id);
+ALTER TABLE electronify.product
+    Add column stock_id varchar(100) references electronify.product_stock (id);
+ALTER TABLE electronify.product
+    Add column product_category varchar(100) references electronify.product_category (id);
+
+ALTER TABLE electronify.payment_methods
+    Add column user_id varchar(100) references electronify.users (id);
+
+ALTER TABLE electronify.orders
+    Add column user_id varchar(100) references electronify.users (id);
+ALTER TABLE electronify.orders
+    Add column payment_id varchar(100) references electronify.payment_details (id);
+
+ALTER TABLE electronify.order_item
+    Add column order_id varchar(100) references electronify.orders (id);
+ALTER TABLE electronify.order_item
+    Add column payment_id varchar(100) references electronify.product (id);
+
+ALTER TABLE electronify.cart
+    Add column session_id varchar(100) references electronify.shopping_session (id);
+ALTER TABLE electronify.cart
+    Add column product_id varchar(100) references electronify.product (id);

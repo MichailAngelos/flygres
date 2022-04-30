e9fbbb1f0738661741f434d02f63f384d2c7b813
Michail Angelos Georgiou <michailangelos.georgiou@gmail.com>
2022-04-30 20:02:48 +0300

    insert products

---
+++ db/electronify/migrations/V1651333374__product_insert.sql
@@ -0,0 +1,22 @@
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('ec1d16e8-818a-4294-8bbe-b968b3f12836', 'Alienware', 'DELL', 2500.0, false, true,'2022-04-30 10:10:25-07','2',0,10,'img/alienware.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('0a35976b-9bf5-4788-a373-908d29a17e20', 'ASUS', 'ASUS', 950.0, false, true,'2022-04-20 10:10:25-07','2',0,15,'img/asus.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('3c0e58e7-442b-4348-aeee-d9d8870d0b60', 'Samsung Smart v8', 'Samsung', 500.0, false, true,'2022-04-20 10:10:25-07','3',0,20,'img/product-1.jpg' );
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('4c33184d-766b-472c-89de-825b5d8889bd', 'MacBook Air', 'Apple', 2000, false, true,'2022-04-20 10:10:25-07','2',0,20 ,'img/product-2.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('ded32baf-1668-4f31-b5cd-2b4ef4ed24a6', 'Iphone 12x', 'Apple', 1200.0, false, true,'2022-04-20 10:10:25-07','3',0,20 ,'img/product-3.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('cf681c42-4ee8-4318-a1f0-56e17fa37bb9', 'Sony Tablet', 'SONY', 250.0, false, false,'2022-04-20 10:10:25-07','3',0,0 ,'img/tablet.png');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('f693ca8e-802a-4303-9dc4-f1fecab0e4c9', 'X-BOX One', 'Microsoft', 800.0, false, true,'2022-04-20 10:10:25-07','1',0,15,'img/xboxOne.jpg' );
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('eadc4ca4-38d7-4071-9a48-14669d48fdbe', 'Sony Smart Ex', 'S0NY', 700.0, false, true,'2022-04-20 10:10:25-07','3',0,10,'img/sonySmart.jpg' );
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('43841a9b-77f0-4b48-bb66-d1d1958bece6', 'Razer Headset', 'Razer', 100.0, false, true,'2022-04-20 10:10:25-07','4',0,50 ,'img/razer.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('0f5107ee-c52c-47b9-b2e8-9263b5910c2b', 'Razer Mouse', 'Razer', 80.0, false, true,'2022-04-20 10:10:25-07','4',0,50 ,'img/razerMouse.jpg');
+INSERT INTO electronify.product (id, name, brand, price, on_sale, in_stock, created_at,product_category,sale,stock,image)
+VALUES ('222649da-4b87-4c89-9608-7528203628fc', 'Logitech Keyboard', 'Logitech', 60.0, false, true,'2022-04-20 10:10:25-07','4',0,50 ,'img/logic.jpg');
